adeaba61bdd677ef9e06a22fcffc37cc3dec3706
phoenix <connorpearson2000@gmail.com>
2020-05-18 21:32:23 +0100

    + Added QR code for user json data (currently encodes hardcoded test data)  + Added third menu button "STATUS COLOUR DESCRIPTION"  + Added and styled "Exposure Status" labels

--- app/src/main/java/com/example/contacttracingapp/MainActivity.java
+++ app/src/main/java/com/example/contacttracingapp/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.contacttracingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.zxing.*;
+import com.google.zxing.common.BitMatrix;
 
 public class MainActivity extends AppCompatActivity {
+    Bitmap bitmap;
+    ImageView QRCodeImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            QRCodeImg = (ImageView)findViewById(R.id.QRCodeImg);
+
+            bitmap = TextToImageEncode("hello");     //TODO enter personal info for encoding
+            QRCodeImg.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+    }
+
+    Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    130, 130, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.black):getResources().getColor(R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 130, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
     }
 }
